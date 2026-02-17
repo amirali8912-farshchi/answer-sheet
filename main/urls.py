df8c5144a91b41bbfd51b063b041d301f9c9f9ea
Amirali Farshchi <amiralifrshchi@gmail.com>
2026-02-17 22:23:36 +0330

    update

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('generate/', views.generate_answer_sheet, name='generate'),
-    path('check/',views.correction,name='check'),
-    path('checking/',views.check,name='checking')
+    path('generate/', views.generate_answer_sheet, name='generate')
 #    path('a/', views.a, name='a'),
 ]
